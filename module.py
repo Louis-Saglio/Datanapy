482a186f330e96ea01e2bea22e4b2113ea9a6c82
Louis-Saglio <louis.saglio@sfr.fr>
2018-04-07 14:15:45 +0200

    [bug] Remove confusion between columns and rows in main

--- module.py
+++ module.py
@@ -2,6 +2,13 @@ import pprint
 import statistics
 
 
+def cast(type_, value, default='null'):
+    try:
+        return type_(value)
+    except ValueError:
+        return default
+
+
 def detect_type(column, null_values=('null', None), types=(float, str)):
     for cell in column:
         if cell in null_values:
@@ -27,10 +34,18 @@ def get_column_stat(column: list, type_, null_values=('null', None)):
     rep = {
         'null_number': sum([column.count(null_value) for null_value in null_values])
     }
-    column = [cell for cell in column if cell not in null_values]
+    error = 'error'
+    column = [cast(type_, cell, error) for cell in column if cell not in null_values]
+    rep['unreadable'] = column.count(error)
+    column = [cell for cell in column if cell != error]
     if type_ is float:
+        for cell in column:
+            assert isinstance(cell, type_), cell
         rep['mean'] = statistics.mean(column)
-        rep['harmonic_mean'] = statistics.harmonic_mean(column)
+        try:
+            rep['harmonic_mean'] = statistics.harmonic_mean(column)
+        except statistics.StatisticsError:
+            rep['harmonic_mean'] = None
         rep['max'] = max(column)
         rep['min'] = min(column)
         rep['median'] = statistics.median(column)
