b41551769ad42477cba78e063eb61bd9270c7a4b
Louis-Saglio <louis.saglio@sfr.fr>
2018-04-07 09:59:45 +0200

    Initial commit

---
+++ module.py
@@ -0,0 +1,55 @@
+import pprint
+import statistics
+
+
+def detect_type(column, null_values=('null', None), types=(float, str)):
+    for cell in column:
+        if cell in null_values:
+            continue
+        types_copy = list(types)
+        for type_ in types:
+            try:
+                type_(cell)
+            except ValueError:
+                types_copy.remove(type_)
+        types = types_copy
+    return types[0]
+
+
+def load_csv(filename):
+    with open(filename) as f:
+        rows = [row.split(',') for row in f.read().split('\n')]
+        return rows.pop(0), rows, [[row[i] for row in rows] for i in range(len(rows))]
+
+
+def get_column_stat(column: list, type_, null_values=('null', None)):
+    rep = {
+        'null_number': sum([column.count(null_value) for null_value in null_values])
+    }
+    column = [cell for cell in column if cell not in null_values]
+    if type_ is float:
+        rep['mean'] = statistics.mean(column)
+        rep['harmonic_mean'] = statistics.harmonic_mean(column)
+        rep['max'] = max(column)
+        rep['min'] = min(column)
+        rep['median'] = statistics.median(column)
+        rep['std_dev'] = statistics.pstdev(column)
+        try:
+            rep['most_frequent'] = statistics.mode(column)
+        except statistics.StatisticsError:
+            rep['most_frequent'] = None
+    elif type_ is str:
+        for value in set(column):
+            rep[value] = column.count(value)
+    return rep
+
+
+def main():
+    headers, data = load_csv('data/boxes.csv')
+    for header, row in zip(headers, data):
+        print(header)
+        pprint.pprint(get_column_stat(row, detect_type(row)))
+
+
+if __name__ == '__main__':
+    main()
